6820a3e85545f3a9e93cf1db4fdc61bd2201e06a
TekapoCat <2018280156@qq.com>
2021-03-07 17:27:33 +0800

    2-7 增加hello测试接口

--- src/main/java/com/loscy/wiki/controller/TestController.java
+++ src/main/java/com/loscy/wiki/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.loscy.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class TestController {
@@ -14,6 +17,11 @@ public class TestController {
      */
     @GetMapping ("/hello")
     public String hello(){
-        return "Hello World！";
+        return "Hello World!";
+    }
+
+    @PostMapping ("/hello/post")
+    public String helloPost(String name){
+        return "Hello World!Post, " + name;
     }
 }
