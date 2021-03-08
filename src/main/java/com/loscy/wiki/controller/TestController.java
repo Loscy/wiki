24c16d0b9d0a52affc3cc1b33ba391fb31eeec15
TekapoCat <2018280156@qq.com>
2021-03-08 20:47:20 +0800

    3-4 mybatis使用：查询test表所有数据

--- src/main/java/com/loscy/wiki/controller/TestController.java
+++ src/main/java/com/loscy/wiki/controller/TestController.java
@@ -1,14 +1,20 @@
 package com.loscy.wiki.controller;
 
+import com.loscy.wiki.domain.Test;
+import com.loscy.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Map;
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class TestController {
+
+    @Resource
+    private TestService testService;
 
     @Value("${test.hello}")
     private String testHello;
@@ -26,5 +32,10 @@ public class TestController {
     @PostMapping ("/hello/post")
     public String helloPost(String name){
         return "Hello World!Post, " + name;
+    }
+
+    @GetMapping ("/test/list")
+    public List<Test> list(){
+        return testService.list();
     }
 }
---
+++ src/main/java/com/loscy/wiki/service/TestService.java
@@ -0,0 +1,19 @@
+package com.loscy.wiki.service;
+
+import com.loscy.wiki.domain.Test;
+import com.loscy.wiki.mapper.TestMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class TestService {
+
+    @Resource
+    private TestMapper testMapper;
+
+    public List<Test> list(){
+        return testMapper.list();
+    }
+}
