ac90cdcb2f6063a1edafefb9f7921ef123739b0c
TekapoCat <2018280156@qq.com>
2021-03-09 15:17:05 +0800

    3-5 增加demo表

---
+++ src/main/java/com/loscy/wiki/service/DemoService.java
@@ -0,0 +1,19 @@
+package com.loscy.wiki.service;
+
+import com.loscy.wiki.domain.Demo;
+import com.loscy.wiki.mapper.DemoMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DemoService {
+
+    @Resource
+    private DemoMapper demoMapper;
+
+    public List<Demo> list(){
+        return demoMapper.selectByExample(null);
+    }
+}
