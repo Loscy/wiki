4c7a731b4cb550557aad46cbc1eda03df68a3d79
TekapoCat <2018280156@qq.com>
2021-03-10 14:24:40 +0800

    3-6 新增resp

---
+++ src/main/java/com/loscy/wiki/service/EbookService.java
@@ -0,0 +1,19 @@
+package com.loscy.wiki.service;
+
+import com.loscy.wiki.domain.Ebook;
+import com.loscy.wiki.mapper.EbookMapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class EbookService {
+
+    @Resource
+    private EbookMapper ebookMapper;
+
+    public List<Ebook> list(){
+        return ebookMapper.selectByExample(null);
+    }
+}
