625e542042a1848f17dd10214a65fdc29dd8c983
TekapoCat <2018280156@qq.com>
2021-03-20 11:24:16 +0800

    7-6 分类选择改为级联选择组件

--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -70,11 +70,12 @@
         <a-form-item label="名称">
           <a-input v-model:value="ebook.name" />
         </a-form-item>
-        <a-form-item label="分类一">
-          <a-input v-model:value="ebook.category1Id" />
-        </a-form-item>
-        <a-form-item label="分类二">
-          <a-input v-model:value="ebook.category2Id" />
+        <a-form-item label="分类">
+          <a-cascader
+            v-model:value="categoryIds"
+            :field-names="{ label: 'name', value: 'id', children: 'children'}"
+            :options="level1"
+          />
         </a-form-item>
         <a-form-item label="描述">
           <a-input v-model:value="ebook.description" type="text" />
@@ -198,11 +199,15 @@ export default defineComponent({
     /*
     * 表单
     * */
-    const ebook = ref({});
+    const categoryIds = ref();
+    const ebook = ref();
     const modalVisible = ref(false);
     const modalLoading = ref(false);
     const handleModalOk = () => {
       modalLoading.value = true;
+
+      ebook.value.category1Id = categoryIds.value[0];
+      ebook.value.category2Id = categoryIds.value[1];
 
       axios.post("/ebook/save", ebook.value).then((response) => {
         const data = response.data;
@@ -228,6 +233,7 @@ export default defineComponent({
     const edit = (record: any) => {
       modalVisible.value = true;
       ebook.value = Tool.copy(record);
+      categoryIds.value = [ebook.value.category1Id, ebook.value.category2Id];
     };
 
     /*
@@ -259,6 +265,31 @@ export default defineComponent({
       })
     };
 
+    const level1 = ref();
+
+    /*
+    * 数据查询
+    * */
+    const handleQueryCategory = () => {
+      loading.value = true;
+      axios.get("/category/all").then((response) => {
+        loading.value = false;
+        const data = response.data;
+        if(data.success) {
+          const categorys = data.content;
+          console.log("原始数据", categorys);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树型结构", level1);
+
+        }else {
+          message.error(data.message);
+        }
+
+      });
+    };
+
     onMounted(() => {
       handleQuery({
         page: 1,
@@ -272,6 +303,7 @@ export default defineComponent({
       pagination,
       columns,
       loading,
+      handleQueryCategory,
       handleTableChange,
       handleQuery,
 
@@ -282,6 +314,8 @@ export default defineComponent({
       modalLoading,
       handleModalOk,
       handleDelete,
+      categoryIds,
+      level1,
 
       ebook
     }
--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -9,13 +9,9 @@
           <template #title>
               <span>
                 <user-outlined/>
-                subnav 1111
+                首页
               </span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
         </a-sub-menu>
         <a-sub-menu key="sub2">
           <template #title>
@@ -72,18 +68,6 @@
 import {defineComponent, onMounted, ref} from 'vue';
 import axios from 'axios';
 
-// const listData: Record<string, string>[] = [];
-// for (let i = 0; i < 23; i++) {
-//   listData.push({
-//     href: 'https://www.antdv.com/',
-//     title: `ant design vue part ${i}`,
-//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-//     description:
-//         'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-//     content:
-//         'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-//   });
-// }
 
 export default defineComponent({
   name: 'Home',
