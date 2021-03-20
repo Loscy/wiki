99337613da0c970109d993dd6f4623b1c7179a60
TekapoCat <2018280156@qq.com>
2021-03-20 13:57:41 +0800

    7-7 首页显示分类菜单

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -4,38 +4,28 @@
       <a-menu
           mode="inline"
           :style="{ height: '100%', borderRight: 0 }"
+          @click="handleClick"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
+        <a-menu-item key="welcome">
+          <router-link :to="'/'">
+            <MailOutlined/>
+            <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in level1" :key="item.id">
+          <template v-slot:title>
               <span>
                 <user-outlined/>
-                首页
+                {{ item.name }}
               </span>
           </template>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-              <span>
-                <laptop-outlined/>
-                subnav 2
-              </span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-              <span>
-                <notification-outlined/>
-                subnav 3
-              </span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutlined/>
+            <span>
+              {{ child.name }}
+            </span>
+          </a-menu-item>
+
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
@@ -67,13 +57,43 @@
 <script lang="ts">
 import {defineComponent, onMounted, ref} from 'vue';
 import axios from 'axios';
+import {Tool} from "@/util/tool";
+import {message} from "ant-design-vue";
 
 
 export default defineComponent({
   name: 'Home',
   setup() {
     const ebooks = ref();
+
+    const level1 =  ref();
+    let categorys: any;
+    /**
+     * 查询所有分类
+     **/
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+          console.log("原始数组：", categorys);
+
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys, 0);
+          console.log("树形结构：", level1.value);
+
+        } else {
+          message.error(data.message);
+        }
+      });
+    };
+
+    const handleClick = () => {
+      console.log("menu click")
+    };
+
     onMounted(() => {
+      handleQueryCategory();
       axios.get("/ebook/list",
           {
             params: {
@@ -88,17 +108,23 @@ export default defineComponent({
 
     return {
       ebooks,
+      level1,
+      handleClick,
+
       pagination: {
         onChange: (page: any) => {
           console.log(page);
         },
         pageSize: 3,
       },
+
+
       actions: [
         { type: 'StarOutlined', text: '156' },
         { type: 'LikeOutlined', text: '156' },
         { type: 'MessageOutlined', text: '8' },
       ],
+
     }
   }
 });
