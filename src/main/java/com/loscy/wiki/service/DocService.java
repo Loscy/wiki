0eb7b54ca62f0852dfbb03ec3b3d6c3dc954d1b7
TekapoCat <2018280156@qq.com>
2021-04-19 16:49:13 +0800

    10-6 增加定时器 定时执行电子书更新SQL，增加日志

--- src/main/java/com/loscy/wiki/service/DocService.java
+++ src/main/java/com/loscy/wiki/service/DocService.java
@@ -183,4 +183,8 @@ public class DocService {
             throw new BusinessException(BusinessExceptionCode.VOTE_REPEAT);
         }
     }
+
+    public void updateEbookInfo() {
+        docMapperCust.updateEbookInfo();
+    }
 }
